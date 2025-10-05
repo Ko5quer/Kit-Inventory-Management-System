a86855ba471b5b42329aa1b926df97019e1061e3
Prince <juniortlou212@gmail.com>
2025-10-05 11:20:16 +0200

    Added sell product function

--- Main.py
+++ Main.py
@@ -12,7 +12,7 @@ class manage_products:
                           )
 
         """)
-        self.curs.execute("CREATE TABLE IF NOT EXISTS Sales(saleID,saleDate,prodName,saleTotal)" )
+        self.curs.execute("CREATE TABLE IF NOT EXISTS Sales(saleID INTEGER PRIMARY KEY AUTOINCREMENT,saleDate,prodName,saleTotal)" )
         self.con.commit()
 
     def addProduct(self):
@@ -36,16 +36,31 @@ class manage_products:
         prodName=input("Enter new product name: ")
         prodPrice=input("Enter new product price: ")
         prodQuantity=input("Enter new product quantity: ")
-        self.curs.execute(f"UPDATE Product SET prodName='{prodName}', prodPrice={prodPrice},prodQuantity={prodQuantity} WHERE prodID={update}")
+        self.curs.execute("UPDATE Product SET prodName=?, prodPrice=?,prodQuantity=? WHERE prodID=?",(prodName, prodPrice, prodQuantity, update))
         self.con.commit()
         print("Data updated succesfully")
 
     def displayProduct(self):
         result=self.curs.execute("SELECT * FROM Product")
-        table=result.fetchall()
-        for row in table:
+        result=result.fetchall()
+        for row in result:
             print(row)
 
+    def sellProduct(self):
+        productID=input("Enter product ID: ")
+        saleDate=input("Enter sale date: ")
+        saleQuantity=int (input("Enter Sale quantity: "))
+        self.curs.execute("SELECT prodQuantity, prodPrice, prodName FROM Product WHERE prodID=?",(productID,))
+        results=self.curs.fetchall()
+        for row in results:
+            quantity=row[0]
+            totQuantity=int(quantity) - saleQuantity
+            totPrice=row[1]*saleQuantity
+            name=row[2]
+        self.curs.execute("INSERT INTO Sales(saleDate,prodName,saleTotal) VALUES(?, ?, ?)",(saleDate, name, totPrice))
+        self.curs.execute("UPDATE Product SET prodQuantity=? WHERE prodID=?",(totQuantity, productID))
+        self.con.commit()
+        self.curs.close
 
 def menu():
     print("\t1.\t Add a product \n\t2.\t Remove a product \n\t3.\t Update a product \n\t4.\t Display all products \n\t5.\t Sell a product \n\t6.\t Exit\n")
@@ -62,5 +77,7 @@ while True:
         manage.updateProduct()
     elif(choice==4):
         manage.displayProduct()
+    elif(choice==5):
+        manage.sellProduct()
 
 
